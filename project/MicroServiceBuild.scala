73f432d28a09bee0e9cc8390c38a1914c82193ff
Les Jones <les.jones@digital.hmrc.gov.uk>
2016-11-02 16:30:59 +0000

    Updated log dependency to fix a Kibana issue

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -31,7 +31,7 @@ private object AppDependencies {
     "uk.gov.hmrc" %% "play-health" % playHealthVersion,
     "uk.gov.hmrc" %% "play-url-binders" % playUrlBindersVersion,
     "uk.gov.hmrc" %% "play-config" % playConfigVersion,
-    "uk.gov.hmrc" %% "logback-json-logger" % playJsonLoggerVersion,
+    "uk.gov.hmrc" %% "play-json-logger" % playJsonLoggerVersion,
     "uk.gov.hmrc" %% "domain" % domainVersion
   )
 
