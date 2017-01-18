36ec16983b957ae0d5c6f17d7c08b74c1bea68b4
Les Jones <les.jones@digital.hmrc.gov.uk>
2017-01-18 13:38:38 +0000

    For E2E added logging - and copyright update

--- app/controllers/NotificationController.scala
+++ app/controllers/NotificationController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 HM Revenue & Customs
+ * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -112,6 +112,7 @@ trait NotificationController extends BaseController {
       case Failure(e) =>
         val message = s"could not parse body due to ${e.getMessage}"
         Logger.warn(message)
+        Logger.info(s"Notification message - ${request.body}")
         Future.successful(
           BadRequest(
             buildFailureResponse(timestampNow, Some(message))
