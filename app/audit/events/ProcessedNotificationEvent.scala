2cd37093f22f67a2db83301f9adaa86c3144a8dd
Jhansi Tummala <jhansi.tummala@digital.hmrc.gov.uk>
2018-05-14 14:12:26 +0100

    Updated dependencies

--- app/audit/events/ProcessedNotificationEvent.scala
+++ app/audit/events/ProcessedNotificationEvent.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -55,4 +55,3 @@ class ProcessedNotificationEvent(auditRef: String, details: ProcessedNotificatio
 object ProcessedNotificationEvent {
   implicit val format = Json.format[ExtendedDataEvent]
 }
-
