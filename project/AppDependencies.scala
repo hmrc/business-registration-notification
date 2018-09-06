54afc14d5338a93ce620844a2b874a863b5df721
david.greatorex <david.greatorex@digital.hmrc.gov.uk>
2018-09-06 10:53:40 +0100

    SCRS-12087 dependency uplift as part of protocol change to https

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -24,7 +24,7 @@ private object AppDependencies {
 object MainDependencies {
   private val microserviceBootstrapVersion  = "6.18.0"
   private val playUrlBindersVersion         = "2.1.0"
-  private val domainVersion                 = "5.1.0"
+  private val domainVersion                 = "5.2.0"
 
   def apply() = Seq(
     "uk.gov.hmrc" %% "microservice-bootstrap" % microserviceBootstrapVersion,
